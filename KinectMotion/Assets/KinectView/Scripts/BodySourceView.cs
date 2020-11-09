077213672c1b675d7d1937dd17378d3222d0d150
Thomas <393890@student.fontys.nl>
2020-11-09 15:20:05 +0100

    plugin, joints code (bodySourceView)

---
+++ KinectMotion/Assets/KinectView/Scripts/BodySourceView.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Kinect = Windows.Kinect;
+using Joint = Windows.Kinect.Joint;
+
+public class BodySourceView : MonoBehaviour 
+{
+    private BodySourceManager mBodySourceManager;
+    public GameObject mJointObject;
+
+    private Dictionary<ulong, GameObject> mBodies = new Dictionary<ulong, GameObject>();
+    private List<Kinect.JointType> _joints = new List<Kinect.JointType>
+    {
+        Kinect.JointType.Head,
+    };
+    
+    void Update () 
+    {
+        #region Get Kinect Data
+        Kinect.Body[] data = mBodySourceManager.GetData();
+        if(data == null)
+        {
+            return;
+        }
+
+        List<ulong> trackedIds = new List<ulong>();
+        foreach (var body in data)
+        {
+            if (body == null)
+                continue;
+
+            if (body.IsTracked)
+                trackedIds.Add(body.TrackingId);
+        }
+            #endregion
+
+        #region Delete Kinect bodies
+        List<ulong> knownIds = new List<ulong>(mBodies.Keys);
+        foreach (ulong trackingId in knownIds)
+        {
+            if (!trackedIds.Contains(trackingId))
+            {
+                Destroy(mBodies[trackingId]);
+
+                mBodies.Remove(trackingId);
+            }
+        }
+        #endregion
+
+        #region Create Kinect bodies
+        foreach (var body in data)
+        {
+            if (body == null)
+                continue;
+
+            if (body.IsTracked)
+            {
+                if (!mBodies.ContainsKey(body.TrackingId))
+                    mBodies[body.TrackingId] = CreateBodyObject(body.TrackingId);
+
+                
+                UpdateBodyObject(body, mBodies[body.TrackingId]);
+            }
+
+        }
+        #endregion
+    }
+
+    private GameObject CreateBodyObject(ulong id)
+    {
+        GameObject body = new GameObject("Body:" + id);
+        
+        foreach (ArticulationJointType joint in _joints)
+        {
+            GameObject newJoint = Instantiate(mJointObject);
+            newJoint.name = joint.ToString();
+
+            newJoint.transform.parent = body.transform;
+        }
+        
+        return body;
+    }
+
+    private void UpdateBodyObject(Kinect.Body body, GameObject bodyObjects)
+    {
+        foreach (Kinect.JointType _joint in _joints)
+        {
+            Joint sourceJoint = body.Joints[_joint];
+            Vector3 targetPosition = GetVector3FromJoint(sourceJoint);
+            targetPosition.z = 0;
+
+            Transform jointObject = bodyObjects.transform.Find(_joint.ToString());
+            jointObject.position = targetPosition;
+        }
+    }    
+    private static Vector3 GetVector3FromJoint(Kinect.Joint joint)
+    {
+        return new Vector3(joint.Position.X * 10, joint.Position.Y * 10, joint.Position.Z * 10);
+    }
+}
