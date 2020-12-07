11a8a03753b53f8d1a19684a5e4ca2f48c77b88b
Dimitri <433046@student.fontys.nl>
2020-12-07 12:11:31 +0100

    fans arduino

---
+++ kinect test2/Assets/fanscontroller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class fanscontroller : MonoBehaviour
+{
+    public FloatValue velocity;
+    public SerialPort serial = new SerialPort("COM7", 9600);
+    private bool lightState = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        CheckVelocity();
+    }
+
+    private void CheckVelocity()
+    {
+        if (velocity.@float > 0)
+        {
+            onLed();
+        }
+        else
+        {
+            offLed();
+        }
+    }
+
+    public void onLed()
+    {
+        if (serial.IsOpen == false)
+        {
+            serial.Open();
+        }
+
+        serial.Write("70");
+        lightState = true;
+    }
+
+    public void offLed()
+    {
+        if (serial.IsOpen == false)
+        {
+            serial.Open();
+        }
+
+        serial.Write("5");
+        lightState = false;
+    }
+}
