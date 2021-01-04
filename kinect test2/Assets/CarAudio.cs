9aca7e443f68d0ebb9077e7081ffa493692dc094
Thomas <393890@student.fontys.nl>
2021-01-04 11:12:18 +0100

    car sounds random pitch and interval, files cleanup

---
+++ kinect test2/Assets/CarAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAudio : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.clip = clip;
+        StartCoroutine(PlaySound());
+    }
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(Random.Range(2f, 12f));
+
+        source.pitch = (Random.Range(0.6f, 1.2f));
+        source.Play();
+
+        yield return new WaitForSeconds(clip.length);
+        StartCoroutine(PlaySound());
+    }
+}
